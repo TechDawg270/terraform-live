1785fe8d506edb3ce8385e5be433975bbdab9d17
Dane Butler <justin.butler270@gmail.com>
2018-01-30 21:33:03 -0700

    formatting to make pretty

--- modules/s3_remote_state/outputs.tf
+++ modules/s3_remote_state/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_arn" {
   description = "The ARN for bucket"
-  value		  = "${aws_s3_bucket.this.arn}"
+  value		    = "${aws_s3_bucket.this.arn}"
 }
 
 output "dynamodb_table" {
   description = "The DynamoDB Table"
-  value		  = "${aws_dynamodb_table.this.name}"
+  value		    = "${aws_dynamodb_table.this.name}"
 }
