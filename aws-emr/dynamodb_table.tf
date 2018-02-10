e160a4a0c14376e999291a9a4712f2dfb1996270
Dane Butler <justin.butler270@gmail.com>
2018-02-10 16:13:39 -0700

    dynamodb table for emr backend checkpointing

---
+++ aws-emr/dynamodb_table.tf
@@ -0,0 +1,22 @@
+resource "aws_dynamodb_table" "emr_kinesis_dynamodb_table" {
+  name           = "EMR-Kinesis"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "HashKey"
+  range_key      = "RangeKey"
+
+  attribute {
+    name = "HaskKey"
+    type = "S"
+  }
+
+  attribute {
+    name = "RangeKey"
+    type = "S"
+  }
+
+  tags {
+    Terraform   = "true"
+    Environment = "${var.environment}"
+  }
+}
