d4929971fdc9c37b0b2a57a2dbebd98dd5b83fde
Dane Butler <justin.butler270@gmail.com>
2018-02-11 11:46:57 -0700

    python script to load temp data to aws kinesis data stream

---
+++ scripts/load_temp_data_to_kinesis.py
@@ -0,0 +1,23 @@
+import json
+import datetime
+import random
+import boto3
+
+kinesis = boto3.client('kinesis', region_name="us-west-2")
+
+def getData(iotName, lowVal, highVal):
+   data = {}
+   data["iotName"] = iotName
+   data["iotValue"] = random.randint(lowVal, highVal)
+   return data
+
+while 1:
+   rnd = random.random()
+   if (rnd < 0.01):
+      data = json.dumps(getData("DemoSensor", 100, 120))
+      kinesis.put_record(StreamName="RawTempData", Data=data, PartitionKey="DemoSensor")
+      print '******** anomaly ******** ' + data
+   else:
+      data = json.dumps(getData("DemoSensor", 10, 20))
+      kinesis.put_record(StreamName="RawTempData", Data=data, PartitionKey="DemoSensor")
+      print data
