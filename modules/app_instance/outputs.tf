735c1a968de570f46bbba144128cf7cc73b47e3c
Dane Butler <justin.butler270@gmail.com>
2018-02-11 21:19:31 -0700

    removing modules directory

--- modules/app_instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ami_name" {
-  description = "The name of the AMI used to launch the instance"
-  value       = "${data.aws_ami.web_app_ami.name}"
-}
-
-output "ec2_id" {
-  description = "The ID of the EC2 instance"
-  value       = "${aws_instance.ec2_instance.id}"
-}
--- modules/s3_remote_state/vars.tf
+++
@@ -1,3 +0,0 @@
-variable "name" {
-  description = "Name to be used for S3 bucket and DynamoDB table creations"
-}
