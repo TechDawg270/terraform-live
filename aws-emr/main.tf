3ecfe019839daa687ddc75168ff586645ccabc6d
Dane Butler <justin.butler270@gmail.com>
2018-02-11 21:20:39 -0700

    switching to convention of hyphens for resource names

--- aws-emr/main.tf
+++ aws-emr/main.tf
@@ -30,7 +30,7 @@ data "terraform_remote_state" "iam" {
 }
 
 resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
+  name        = "allow-all"
   description = "Allow all inbound traffic"
   vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
 
