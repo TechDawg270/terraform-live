f1f9cf60b798bb035ff1b1a6b48c913312dd3bcb
Dane Butler <justin.butler270@gmail.com>
2018-02-13 19:02:45 -0700

    adjusting auto-scaling properties and tags

--- app-instance/autoscaling.tf
+++ app-instance/autoscaling.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "${var.role}*"
+  name_regex  = "${var.role}-*"
   owners      = ["self"]
 }
 
@@ -9,7 +9,8 @@ resource "aws_launch_configuration" "app_launchconfig" {
   image_id             = "${data.aws_ami.ami.id}"
   instance_type        = "${var.instance_type}"
   security_groups      = ["${aws_security_group.app_instance.id}"]
-  user_data            = "INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)\necho 'this is: '$INSTANCE_IP > /var/www/html/index.html"
+  key_name             = "${var.key_name}"
+  user_data            = "${file("./scripts/nginx_set_homepage.sh")}"
 
   lifecycle {
       create_before_destroy = true
@@ -20,15 +21,26 @@ resource "aws_autoscaling_group" "app_autoscaling_group" {
   name                      = "${var.environment}-autoscaling-group"
   vpc_zone_identifier       = ["${data.terraform_remote_state.vpc.vpc_public_subnets}"]
   launch_configuration      = "${aws_launch_configuration.app_launchconfig.name}"
-  min_size                  = 2
+  min_size                  = 1
   max_size                  = 5
   health_check_grace_period = 300
   health_check_type         = "ELB"
   load_balancers            = ["${aws_elb.app_elb.name}"]
   force_delete              = true
 
-  tags {
-      Terraform           = "true"
-      propagate_at_launch = true
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = "${var.environment}"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
   }
 }
