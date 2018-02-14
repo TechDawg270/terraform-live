c1e69999a9c8f79a1dd475821616dbced38b817e
Dane Butler <justin.butler270@gmail.com>
2018-02-14 09:04:08 -0700

    moved temp data stream to iot-temp-data project to illustrate per project infra management and state isolation

--- aws-kinesis/main.tf
+++ aws-kinesis/main.tf
@@ -11,17 +11,6 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_kinesis_stream" "raw_temp_data_stream" {
-  name             = "iot-raw-temp-data"
-  shard_count      = 1
-  retention_period = "${var.retention_period}"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "${var.environment}"
-  }
-}
-
 resource "aws_kinesis_stream" "raw_apache_log_data_stream" {
   name             = "raw-apache-log-data"
   shard_count      = 2
