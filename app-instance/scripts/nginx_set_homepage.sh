2cdc2df36e4f524081657c6457a08a67e16d981f
Dane Butler <justin.butler270@gmail.com>
2018-02-13 19:24:49 -0700

    changing to public ip to grab for ssh session

--- app-instance/scripts/nginx_set_homepage.sh
+++ app-instance/scripts/nginx_set_homepage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
-sudo echo 'this is: '$INSTANCE_IP > /var/www/html/index.html
+sudo echo 'this is (public IP): '$INSTANCE_IP > /var/www/html/index.html
